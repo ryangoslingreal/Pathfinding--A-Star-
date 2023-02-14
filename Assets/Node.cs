97915e2bedd2d44aaf0771989df503518fbee46f
ryangoslingreal <alexander.sokolovich05@gmail.com>
2023-02-14 12:53:54 +0000

    dsfghj

--- Assets/Node.cs
+++ Assets/Node.cs
@@ -6,4 +6,10 @@ public class Node
 {
     public bool traversable;
     public Vector3 worldPos;
+
+    public Node(bool _traversable, Vector3 _worldPos)
+    {
+        traversable = _traversable;
+        worldPos = _worldPos;
+    }
 }
